f68f5d6f2fffed1c590720dd38b36ee1fb938eef
Tubaleviao <alvaro.prata.filho@gmail.com>
2019-08-26 14:51:35 +0000

    initial stuff

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const router = require('./routes')
 const app = express()
 const port = 80
 
+app.use('/', router)
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('home'))
+app.get('*', (req, res) => res.status(404))
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
